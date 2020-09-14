d687ace0e284cb2732d42563294c8efd4c2eb6eb
Luke Fox <lukefox5@gmail.com>
2020-09-14 19:26:21 +0100

    Added basic frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/home/Home';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className='App'>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Switch></Switch>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
