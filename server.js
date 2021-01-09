6d0d37dc1f21e99374d19495cf8b051a1ea7b867
Luke Fox <lukefox5@gmail.com>
2021-01-09 19:52:57 +0000

    Added missing path dependency

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 require('dotenv').config();
 
